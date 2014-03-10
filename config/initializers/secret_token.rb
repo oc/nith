5a51ff05690777e3928ebc730d0853d84c36b045
Ole Christian Rynning <oc@rynning.no>
2014-03-10 14:55:34 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nith::Application.config.secret_key_base = '44ad36ddb302100668c4b0bcdc18d50574eb824ac276052068af1c2322f0166618c6b6e24339b6d63c2d533ec19a023af47a696d120a33f200782bf2eacf60a5'
